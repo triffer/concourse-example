f0dac3c17d5c6abf94c3da356c80152567fd1ea3
triffer <tim.riffer@traversals.com>
2019-10-12 13:14:08 +0200

    Added new pipeline

--- manual.sh
+++ manual.sh
@@ -9,14 +9,21 @@ fly --target test login --concourse-url http://localhost:8080 -u test -p test
 # Show target config
 cat ~/.flyrc
 
+
 # Configure basic pipeline and login to Concourse
 fly -t test set-pipeline -c ./pipelines/basic-pipeline.yaml -p hello-world
-
 # Update basic pipeline
 fly -t test set-pipeline -c ./pipelines/basic-pipeline_v2.yaml -p hello-world
 
-# Configure pipeline resources
-fly -t test set-pipeline -c ./pipelines/pipeline_resources.yaml -p pipeline-resources
 
+# Configure pipeline resources
+fly -t test set-pipeline -c ./pipelines/pipeline-resources.yaml -p pipeline-resources
 # Update pipeline resources with trigger to automatically start on new version
-fly -t test set-pipeline -c ./pipelines/pipeline_resources_v2.yaml -p pipeline-resources
+fly -t test set-pipeline -c ./pipelines/pipeline-resources_v2.yaml -p pipeline-resources
+
+
+# Configure pipeline resources
+fly -t test set-pipeline -c ./pipelines/task-outputs.yaml -p task-outputs
+
+# Configure pipeline resources
+fly -t test sp -c ./pipelines/pipeline-parameters.yaml -p pipeline-parameters
